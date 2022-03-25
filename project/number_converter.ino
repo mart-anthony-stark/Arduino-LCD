3e6269a40d000691f64fe43f2e6e125ba82e88aa
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-25 21:12:38 +0800

    add input catch

--- project/number_converter.ino
+++ project/number_converter.ino
@@ -1,6 +1,6 @@
 int step = 0;
 int num = 0;
-int temp = 0;
+String numSystem = "";
 void setup()
 {
     Serial.begin(9600);
@@ -12,21 +12,48 @@ void loop()
 
     while (Serial.available())
     {
-        String a = Serial.readString(); // read the incoming data as string
+        String input = Serial.readString(); // read the incoming data as string
 
         // Handles input for number to be converted
         if (step == 0)
         {
-            Serial.print(a + "\n");
-            num = a.toInt();
-            Serial.print("Enter number system: ");
-            step++;
+            Serial.print(input + "\n");
+            Serial.println(isValidNumber(input));
+
+            if (!isValidNumber(input))
+            {
+                Serial.println("Invalid input. Characters must be numeric");
+                Serial.print("Enter number to be converted: ");
+            }
+            else
+            {
+                num = input.toInt();
+                Serial.print("Enter number system: ");
+                step++;
+            }
         }
         // Handles input for number system
         else if (step == 1)
         {
-            Serial.print(a + "\n");
+            Serial.print(input + "\n");
+            numSystem = input;
             step++;
         }
+        // else if (step == 2)
+        // {
+        //     switch(){
+
+        //     }
+        // }
     }
+}
+
+boolean isValidNumber(String str)
+{
+    for (byte i = 0; i < str.length(); i++)
+    {
+        if (isDigit(str.charAt(i)))
+            return true;
+    }
+    return false;
 }
